38f59fc5757d837ef7bd27e9446375fef2ceb6be
Mihaly Virag <virag.mihaly@gmail.com>
2017-05-01 21:33:50 +0200

    - initizilize java backend Application

---
+++ src/main/java/org/myhu/petstore/initApplication.java
@@ -0,0 +1,36 @@
+package org.myhu.petstore;
+
+import org.myhu.petstore.model.Pet;
+import org.myhu.petstore.repo.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class initApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(initApplication.class, args);
+	}
+
+	@Autowired
+	PetRepository petRepository;
+
+	@Bean
+	public String initTemplate()
+	{
+		petRepository.save(createPet());
+
+		return "";
+	}
+
+	private Pet createPet() {
+		Pet pet = new Pet();
+		pet.setName("myhu");
+		pet.setCategory("small");
+		pet.setUri("https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSLS-WZEbGr1nPAs2aAhBDvMqIPKCvKlP2AvmVPVgD5r0IkzLxf");
+		return pet;
+	}
+
+}
